b78cb7c5f958bda4a6b6a11f1498081016c61e29
xiangsong <1638392300@qq.com>
2020-10-20 00:08:26 +0800

    fix

--- spring-01-ioc/src/main/java/com/xschen/spring/annotation/i_propertyyml/PropertySourceYmlApplication.java
+++ spring-01-ioc/src/main/java/com/xschen/spring/annotation/i_propertyyml/PropertySourceYmlApplication.java
@@ -1,5 +1,6 @@
 package com.xschen.spring.annotation.i_propertyyml;
 
+import com.xschen.spring.annotation.i_propertyyml.bean.JdbcYmlProperty;
 import com.xschen.spring.annotation.i_propertyyml.config.JdbcYmlConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,6 +12,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class PropertySourceYmlApplication {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JdbcYmlConfiguration.class);
-        
+        System.out.println(ctx.getBean(JdbcYmlProperty.class).toString());
     }
 }
--- spring-01-ioc/src/main/java/com/xschen/spring/annotation/i_propertyyml/config/JdbcYmlConfiguration.java
+++ spring-01-ioc/src/main/java/com/xschen/spring/annotation/i_propertyyml/config/JdbcYmlConfiguration.java
@@ -1,12 +1,17 @@
 package com.xschen.spring.annotation.i_propertyyml.config;
 
+import com.xschen.spring.annotation.i_propertyyml.factory.YmlPropertySourceFactory;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author xschen
  */
 
 @Configuration
-
+@ComponentScan("com.xschen.spring.annotation.i_propertyyml.bean")
+@PropertySource(value = "classpath:annotation/propertyyml/jdbc.yml",
+        factory = YmlPropertySourceFactory.class)
 public class JdbcYmlConfiguration {
 }

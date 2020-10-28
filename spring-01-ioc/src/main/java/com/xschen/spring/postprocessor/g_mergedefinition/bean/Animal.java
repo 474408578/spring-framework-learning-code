f1ccc691be1265f84bbedc85c850841b1bf0b68d
xschen <xschen@trip.com>
2020-10-28 21:27:02 +0800

    fix

--- spring-01-ioc/src/main/java/com/xschen/spring/postprocessor/g_mergedefinition/bean/Animal.java
+++ spring-01-ioc/src/main/java/com/xschen/spring/postprocessor/g_mergedefinition/bean/Animal.java
@@ -4,19 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author xschen
+ *
+ * postProcessMergedBeanDefinition 发生在Bean的实例化之后，自动注入之前，
+ * 这样可以在属性赋值和自动注入之前，把要注入的属性收集好。
  */
 
 
 public abstract class Animal {
 
-    @Autowired
+//    @Autowired
     private Person person;
 
     public Person getPerson() {
         return person;
     }
 
+    @Autowired
     public void setPerson(Person person) {
+        System.out.println("Animal setPerson run ……");
         this.person = person;
     }
+
 }

43b3ae52b0e60884499ff086996ed6357b1731d8
xschen <xschen@trip.com>
2021-01-13 19:48:02 +0800

    fix

--- spring-01-ioc/src/main/java/com/xschen/spring/container/a_beanfactory/ListableBeanFactoryApplication.java
+++ spring-01-ioc/src/main/java/com/xschen/spring/container/a_beanfactory/ListableBeanFactoryApplication.java
@@ -17,8 +17,10 @@ import java.util.stream.Stream;
 public class ListableBeanFactoryApplication {
 
     public static void main(String[] args) {
-        ClassPathResource resource = new ClassPathResource("container/listable-container.xml");
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 加载并解析指定XML文件的bean
+        ClassPathResource resource = new ClassPathResource("container/listable-container.xml");
         XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
         beanDefinitionReader.loadBeanDefinitions(resource);
 
@@ -34,7 +36,9 @@ public class ListableBeanFactoryApplication {
         // ListableBeanFactory 在获取容器内所有 Bean 时真的不会把这些手动注册的拿出来()
         // getBeanNamesOfType 和 getBeansOfType 除外
         Stream.of(beanFactory.getBeanDefinitionNames()).forEach(System.out::println);
+
         System.out.println("容器中真的有注册Dog: " + beanFactory.getBean(Dog.class));
+
         System.out.println("容器中的所有Dog：" + Arrays.toString(beanFactory.getBeanNamesForType(Dog.class)));
 
     }

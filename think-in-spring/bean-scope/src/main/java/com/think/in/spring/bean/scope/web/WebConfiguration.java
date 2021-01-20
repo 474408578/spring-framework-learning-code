65dcc8ab5b326d206f459949b61000e35f82a404
xiangsong <1638392300@qq.com>
2021-01-20 22:35:41 +0800

    fix

--- think-in-spring/bean-scope/src/main/java/com/think/in/spring/bean/scope/web/WebConfiguration.java
+++ think-in-spring/bean-scope/src/main/java/com/think/in/spring/bean/scope/web/WebConfiguration.java
@@ -4,17 +4,20 @@ import com.think.in.spring.ioc.container.overview.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
+ * Web MVC 配置类
  * @author xschen
  */
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc // 激活Spring MVC
 public class WebConfiguration {
 
     @Bean
+//    @RequestScope // 作用域
     @ApplicationScope
     public User user() {
         User user = new User();
